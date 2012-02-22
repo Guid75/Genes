50e26fda621445e527a3e38125258e4f86b3d05b
Guillaume Denry <guillaume.denry@gmail.com>
2012-02-22 23:17:41 +0000

    the message issued from the input text are is sent to the server and area is cleared

--- client/main.js
+++ client/main.js
@@ -1,3 +1,28 @@
+function sendAndClear(inputEl, socket) {
+    var value = inputEl.value;
+    var syntaxError;
+    try {
+        var obj = eval('(' + value + ')');
+        socket.emit('message', obj);
+        console.log(obj);
+
+        inputEl.value = '';
+        document.getElementById("syntax-error").hidden = true;
+    } catch (e){
+        syntaxError = document.getElementById("syntax-error");
+        syntaxError.innerHTML = e.message;
+        syntaxError.hidden = false;
+    }
+}
+
+function upHistory(){
+    console.log('up');
+}
+
+function downHistory(){
+    console.log('down');
+}
+
 window.addEventListener('load', function () {
     var socket = io.connect('http://localhost');
     var outputEl = document.getElementById("textarea-output");
@@ -7,8 +32,25 @@ window.addEventListener('load', function () {
     socket.on('disconnect', function(){
         outputEl.value += "Disconnected from the game server\n";
     });
-    socket.on('news', function (data) {
-        console.log(data);
-        socket.emit('my other event', { my: 'data' });
-    });
+
+    var inputEl = document.getElementById("textarea-input");
+    inputEl.onkeypress = function(event){
+        if (event.keyCode === 13 && event.shiftKey){
+            sendAndClear(inputEl, socket);
+            event.returnValue = false;
+            return;
+        }
+    };
+    inputEl.onkeydown = function(event){
+        if (event.keyCode === 38 && event.ctrlKey) {
+            upHistory();
+            event.returnValue = false;
+        } else if (event.keyCode === 40 && event.ctrlKey) {
+            downHistory();
+            event.returnValue = false;
+        }
+    };
+    document.getElementById("send-button").onclick = function(){
+        sendAndClear(inputEl, socket);
+    };
 }, false);
--- server/server.js
+++ server/server.js
@@ -53,9 +53,8 @@ app.listen(port);
 // only for messages now
 var io = socketio.listen(app);
 io.sockets.on('connection', function (socket) {
-    console.log("connection");
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
+    console.log('connection');
+    socket.on('message', function(data){
         console.log(data);
     });
 });
