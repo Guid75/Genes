7c38504f6bd304ed4f733e370e7688918a7e90a4
Guillaume Denry <guillaume.denry@gmail.com>
2012-03-26 12:48:55 +0000

    cleaner code

---
+++ client/game.js
@@ -0,0 +1,8 @@
+Game.manageSessionMessage = function(data) {
+    if (data.event === 'list') {
+        Game.mainMenu.refreshAll(data.sessions);
+        Game.mainMenu.refreshServer(0, { players: 4, running: true });
+    } else if (data.event === 'refresh') {
+        Game.mainMenu.refreshServer(data.index, data.server);
+    }
+};
